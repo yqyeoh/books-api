645a41316791b1963142b646dee278c397816e13
Angeline <atan@thoughtworks.com>
2019-03-19 20:04:22 +0800

    Trigger create tables before express app listen

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const app = require("./app");
+const { sequelize } = require("./models");
 
 const port = process.env.PORT || 5555;
+const eraseDatabaseOnSync = true;
 
-app.listen(port, () => {
-  if (process.env.NODE_ENV === "production") {
-    console.log(`Server is running on Heroku with port number ${port}`);
-  } else {
-    console.log(`Server is running on http://localhost:${port}`);
-  }
+sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
+  app.listen(port, () => {
+    if (process.env.NODE_ENV === "production") {
+      console.log(`Server is running on Heroku with port number ${port}`);
+    } else {
+      console.log(`Server is running on http://localhost:${port}`);
+    }
+  });
 });
