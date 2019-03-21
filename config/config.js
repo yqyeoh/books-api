7fde4977b524c3826d7bc83671bd7d747ac14ab7
Angeline <atan@thoughtworks.com>
2019-03-22 03:36:36 +0800

    Modify prod credentials

--- config/config.js
+++ config/config.js
@@ -18,9 +18,7 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
     options: {
       dialect: "postgres"
     }
--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
-
+let sequelize;
 //Connect to different database depending on env
-const sequelize = new Sequelize(config.database, config.username, config.password, config.options);
+if(env === "production"){
+  sequelize = new Sequelize(config.url, config.options);
+} else{
+  sequelize = new Sequelize(config.database, config.username, config.password, config.options);
+}
 
 const models = {
   Book: sequelize.import("./book"),
